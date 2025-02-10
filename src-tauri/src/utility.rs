12708f5bc3cd143d7b5bda150f8586b62c5ad1bd
Eigeen <dengyk2002@qq.com>
2025-02-10 16:57:14 +0800

    message box for windows

--- src-tauri/src/utility.rs
+++ src-tauri/src/utility.rs
@@ -1,9 +1,10 @@
-use windows::{
-    core::{w, PCWSTR},
-    Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_ICONERROR},
-};
-
+#[cfg(target_os = "windows")]
 pub fn message_box_error(message: &str) {
+    use windows::{
+        core::{w, PCWSTR},
+        Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_ICONERROR},
+    };
+
     let msg = to_utf16_with_nul(message);
     unsafe {
         MessageBoxW(None, PCWSTR(msg.as_ptr()), w!("Ree Pak GUI Error"), MB_ICONERROR);
