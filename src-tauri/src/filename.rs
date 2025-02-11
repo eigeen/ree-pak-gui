f60ba877212fc23ff87b33ab6a64c23b20ab3b50
Eigeen <dengyk2002@qq.com>
2025-02-11 11:54:56 +0800

    support .list.zst format filelist

--- src-tauri/src/filename.rs
+++ src-tauri/src/filename.rs
@@ -15,7 +15,16 @@ impl FileListInfo {
         for entry in std::fs::read_dir(path)? {
             let entry = entry?;
             let path = entry.path();
-            if !path.is_file() || path.extension().unwrap_or_default() != "list" {
+            let full_ext = path
+                .iter()
+                .last()
+                .unwrap_or_default()
+                .to_string_lossy()
+                .split('.')
+                .last()
+                .unwrap_or_default()
+                .to_lowercase();
+            if !path.is_file() || [".list", ".list.zst"].contains(&full_ext.as_str()) {
                 continue;
             }
             let name = path.file_name().unwrap_or_default().to_string_lossy().to_string();
@@ -24,5 +33,16 @@ impl FileListInfo {
         }
 
         Ok(files)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_walk_dir() {
+        let files = FileListInfo::walk_dir("../../assets/filelist").unwrap();
+        eprintln!("files: {:#?}", files);
     }
 }
