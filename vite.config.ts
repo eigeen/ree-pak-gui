5173453cd2cdd5f10510c132975e91e68970917e
Eigeen <dengyk2002@qq.com>
2026-03-30 23:39:05 +0800

    fix: fix css color transformation issues

--- vite.config.ts
+++ vite.config.ts
@@ -67,12 +67,18 @@ export default defineConfig({
     'TAURI_PLATFORM_TYPE',
     'TAURI_DEBUG'
   ],
+  css: {
+    transformer: 'lightningcss',
+    devSourcemap: true
+  },
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    minify: !process.env.TAURI_DEBUG ? 'oxc' : false,
     // 为调试构建生成源代码映射 (sourcemap)
-    sourcemap: !!process.env.TAURI_DEBUG
+    sourcemap: !!process.env.TAURI_DEBUG,
+    // To avoid color transformation issues
+    cssMinify: 'lightningcss'
   }
 })
